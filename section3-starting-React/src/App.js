fd79b4b0587316b0c9c9b4421cb4bc2153a4c9af
HYEMBURGERWORLD <burgerhyem@gmail.com>
2023-06-10 23:13:40 +0900

    ✨

--- section3-starting-React/src/App.js
+++ section3-starting-React/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
+      <h2>Hello React!</h2>
+      <ExpenseItem />
     </div>
   );
 }
---
+++ section3-starting-React/src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.59;
+
+  return (
+    <div className='expense-item'>
+      {/* date 객체이기 때문에 문자열로 출력 불가능 -> Error -> toISOString 메서드 사용 */}
+      <div>{expenseDate.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
