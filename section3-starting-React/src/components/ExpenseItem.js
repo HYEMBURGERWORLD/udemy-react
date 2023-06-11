6549dea5b7e1c33f05651d7d14c2249eb9e15162
HYEMBURGERWORLD <burgerhyem@gmail.com>
2023-06-11 15:50:18 +0900

    props

--- section3-starting-React/src/components/ExpenseItem.js
+++ section3-starting-React/src/components/ExpenseItem.js
@@ -1,17 +1,16 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.59;
+function ExpenseItem(props) {
+  // props는 어떤 이름으로 사용해도 되지만 보통 정확한 의미 전달을 위해 props를 사용한다
+  // 우리가 얻는 속성에 대한 모든 값을 가진 객체를 의미한다.
 
   return (
     <div className='expense-item'>
       {/* date 객체이기 때문에 문자열로 출력 불가능 -> Error -> toISOString 메서드 사용 */}
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
